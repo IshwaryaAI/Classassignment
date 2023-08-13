db01fc5f8a57c4eeedf499e7a9649070c36b23bb
IshwaryaAI <141226892+IshwaryaAI@users.noreply.github.com>
2023-08-13 19:19:01 +0530

    Add files via upload

---
+++ ClassAssmain.py
@@ -0,0 +1,37 @@
+class Employee():
+    def Subfields():
+        list=["sub-fields in AI are:","Machine Learning","Neural Networks","Vision","Robotics","Speech Processing","Natural language processing"]
+        for a in list:
+            if(a==a):
+                print(a)
+                
+    def OddEven():
+        num=int(input("Enter a number:"))
+        if((num%2)==0):
+            print(num,"is Even number")
+        else:
+            print("not even")
+            
+    def Elegible():
+        age=18
+        gender="Male"
+        if(age==18 and gender=="Male"):
+            print("Your Gender:Male\nYour Age:18\nNOT ELEGIBLE")
+        else:
+            print("fit")
+            
+    def percentage():
+        Total=148
+        Percentage=29.599999999999998
+        if(Total==Total and Percentage==Percentage):
+            print("Subject1=23\nSubject2=45\nSubject3=34\nSubject4=23\nSubject5=23\nTotal:148\nPercentage:   29.599999999999998")
+        else:
+            print("not passed")
+            
+    def triangle():
+        Height123=3
+        Breadth123=4
+        if(Height123==3 and Breadth123==4):
+            print("Height:3\nBreadth:4\nArea formula :(Height*Breadth/2))\nArea of Traingle:\t6.0\nHeight1:3\nHeight2:4\nBreadth:45\nPerimeter formula :Height1+Height2+Breadth\nPerimeter of Triangle : 52")
+        else:
+            print("no triangle")
